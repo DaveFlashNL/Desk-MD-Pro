d244d40f38382279acbfd717f320a232446db406
Dave Wouters <ddw@ddw.nu>
2022-12-26 15:33:23 +0100

    smallfix for mac devs

--- build-renderer.sh
+++ build-renderer.sh
@@ -8,8 +8,8 @@ rm -rf WebMDPro
 
 git clone https://github.com/DaveFlashNL/WebMDPro
 cd WebMDPro
-npm i
+npm i --legacy-peer-deps
 PUBLIC_URL="sandbox://" REACT_APP_NO_GA_RELEASE="true" npm run build; rm -rf ../renderer; cp -rv build ../renderer
 cd ..
 
-rm -rf WebMDPro
+#rm -rf WebMDPro
