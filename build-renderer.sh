4b95de09c217b9a3c0b663144520c5d21d8f4d74
Dave Wouters <ddw@ddw.nu>
2023-01-24 11:28:05 +0100

    update some stuffs

--- build-renderer.sh
+++ build-renderer.sh
@@ -9,7 +9,7 @@ rm -rf WebMDPro
 git clone https://github.com/DaveFlashNL/WebMDPro
 cd WebMDPro
 npm i --legacy-peer-deps
-PUBLIC_URL="sandbox://" REACT_APP_NO_GA_RELEASE="true" npm run build; rm -rf ../renderer; cp -rv build ../renderer
+PUBLIC_URL="sandbox://" npm run build; rm -rf ../renderer; cp -rv build ../renderer
 cd ..
 
-#rm -rf WebMDPro
+rm -rf WebMDPro
